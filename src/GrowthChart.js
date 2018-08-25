81902626c2bcca5c44ac4eefabb3e0a647ddfa49
Daniel Greene <daniel.b.greene@gmail.com>
2018-08-25 00:31:09 -0400

    refactoring

--- src/GrowthChart.js
+++ src/GrowthChart.js
@@ -36,7 +36,7 @@ export default props => {
             name={set.description}
             data={set.data}
             line
-            fill='black'
+            fill={set.color || 'black'}
           />
         ))}
         <Scatter
